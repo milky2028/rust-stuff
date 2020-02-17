8b27c58fbf3667a2ad00440039c84941a7d7a770
Tyler <tylergross28@gmail.com>
2020-02-16 22:30:40 -0600

    12 days of christmas program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+fn print_empty_line() {
+    println!("");
+}
+
 fn print_day_of_christmas(day: u32) {
     let converted_day = match day {
         1 => "first",
@@ -19,8 +23,23 @@ fn print_day_of_christmas(day: u32) {
     println!("My true love gave to me:");
 }
 
-fn print_empty_line() {
-    println!("");
+fn print_gift(day: u32) {
+    let gift = match day {
+        2 => "Two turtle doves,",
+        3 => "Three French hens,",
+        4 => "Four calling birds,",
+        5 => "Five golden rings,",
+        6 => "Six geese laying,",
+        7 => "Seven swans a swimming,",
+        8 => "Eight maids a milking,",
+        9 => "Nine ladies dancing",
+        10 => "Ten lords a leaping",
+        11 => "Eleven pipers piping ",
+        12 => "Twelve drummers drumming",
+        _ => return,
+    };
+
+    println!("{}", gift);
 }
 
 fn print_closing_line(day: u32) {
@@ -32,8 +51,11 @@ fn print_closing_line(day: u32) {
 
 fn main() {
     print_empty_line();
-    for day in 1..13 {
+    for day in 1..=12 {
         print_day_of_christmas(day);
+        for gift_day in (1..=day).rev() {
+            print_gift(gift_day);
+        }
         print_closing_line(day);
         print_empty_line();
     }
